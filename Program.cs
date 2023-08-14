ce8d41f43ff6fbf08d0183c8fe9d9c4ee4f0b624
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-14 15:48:14 +0530

    Welcome to ModeAnalyzer

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ModeAnalyzer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MOdeAnalyzer");
+        }
+    }
+}
