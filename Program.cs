cef9b6cbe2e6c99fc1c68de2c3533d1edde0ddd7
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-14 16:19:12 +0530

    Welcome to RepeatTC1.1

--- Program.cs
+++ Program.cs
@@ -1,20 +1,8 @@
 ﻿using System;
 
-class MoodAnalysisException : Exception
-{
-    public MoodAnalysisException(string message) : base(message)
-    {
-    }
-}
-
 class MoodAnalyzer
 {
     private string message;
-
-    public MoodAnalyzer()
-    {
-        message = "I am in Any Mood";
-    }
 
     public MoodAnalyzer(string message)
     {
@@ -23,20 +11,12 @@ class MoodAnalyzer
 
     public string AnalyseMood()
     {
-        try
+        if (message.Contains("Sad"))
         {
-            if (message.Contains("Sad"))
-            {
-                throw new MoodAnalysisException("Mood is sad, but handling the exception.");
-            }
-            else
-            {
-                return "HAPPY";
-            }
+            return "SAD";
         }
-        catch (MoodAnalysisException ex)
+        else
         {
-            Console.WriteLine("Exception: " + ex.Message);
             return "HAPPY";
         }
     }
@@ -46,12 +26,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        MoodAnalyzer defaultMoodAnalyzer = new MoodAnalyzer();
-        string mood1 = defaultMoodAnalyzer.AnalyseMood();
-        Console.WriteLine("Mood 1: " + mood1);
-
-        MoodAnalyzer customMoodAnalyzer = new MoodAnalyzer("Feeling Sad");
-        string mood2 = customMoodAnalyzer.AnalyseMood();
-        Console.WriteLine("Mood 2: " + mood2);
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        string mood = moodAnalyzer.AnalyseMood();
+        Console.WriteLine("Mood: " + mood);
     }
 }
